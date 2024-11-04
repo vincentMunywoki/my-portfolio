0eca870e4ee5fa8c867f696520be1b1688a3dcde
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-04 14:49:30 +0300

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+
+const App = () => {
+  return (
+    <div>
+      {/*navbar mounting.*/}
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+// type rafce to convert navbar file to component.
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt="" />   
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul> 
+        <div className="nav-connect">Connect with me Now.</div>
+    </div>
+  )
+}
+
+export default Navbar

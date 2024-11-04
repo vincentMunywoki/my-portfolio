344e94c0e02e285d36bffc494de6de03d17bd8aa
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-04 16:06:35 +0300

    my first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 // type rafce to convert navbar file to component.
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.jpg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <img src={logo} alt="" />   
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul> 
-        <div className="nav-connect">Connect with me Now.</div>
-    </div>
-  )
-}
+    <div className="navbar">
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" />
+      </div>
 
-export default Navbar
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with Me</div>
+    </div>
+  );
+};
+
+export default Navbar;

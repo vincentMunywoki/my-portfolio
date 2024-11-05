cea9b9a4c71d331356b19121ef04737f01d2c31a
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-05 13:56:37 +0300

    footer-update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import logo from "../../assets/logo.png";
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer-top">
             <div className="footer-top-left">
-                <img src={footer_logo }alt="" />
+                <img src={logo }alt="" />
                 <p>I am a tech enthusiast with a strong belief in the transformative power of technology to shape the future. </p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <p>Suggest an Update</p>
+                    <img src={user_icon} alt="" />
+                    <textarea name="text" rows="5" placeholder='Share your suggeston here...'></textarea>
+                </div>
+                <div className="foter-subscribe">Subscribe</div>
+            </div>        
+        </div>
+        <hr />
+        <div className="footer-bottom">
+            <p className="footer-bottom-left">@ 2024 Vincent Munywoki. All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term of services</p>
+                <p>Privacy policy</p>
+                <p>Connect with me</p>
             </div>
         </div>
     </div>

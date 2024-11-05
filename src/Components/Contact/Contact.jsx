f16dd63f3950b3f1378f8389075da02791d0721e
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-05 11:35:40 +0300

    Contact me

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -31,6 +31,15 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
+            <form className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder='Enter your name' name='name'/>
+                <label htmlFor="">Your Email</label>
+                <input type="email" placeholder='Enter your email' name='email' />
+                <label htmlFor="">Write your message here</label>
+                <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
         </div>
     </div>
   )

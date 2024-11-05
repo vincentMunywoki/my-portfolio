20d8403cf9fa3f1f425e473dd9aa98c89d32d8c1
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-05 14:53:32 +0300

    footer-update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,11 +15,11 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li><p onClick={()=>setMenu("home")} >Home</p>{menu==="home"? <img src={underline} alt=''/>:<></>}</li>
-        <li><p onClick={()=>setMenu("about")} >About Me</p>{menu==="about"? <img src={underline} alt=''/>:<></>}</li>
-        <li><p onClick={()=>setMenu("services")} >Services</p>{menu==="services"? <img src={underline} alt=''/>:<></>}</li>
-        <li><p onClick={()=>setMenu("portflio")} >Portfolio</p>{menu==="work"? <img src={underline} alt=''/>:<></>}</li>
-        <li><p onClick={()=>setMenu("contact")} >Contact</p>{menu==="contact"? <img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("home")} >Home</p>{menu==="home"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("about")} >About Me</p>{menu==="about"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("services")} >Services</p>{menu==="services"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("work")} >Portfolio</p>{menu==="work"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("contact")} >Contact</p>{menu==="contact"?<img src={underline} alt=''/>:<></>}</li>
       </ul>
       <div className="nav-connect">Connect with Me</div>
     </div>

c1a0f87c0ff5ff776f47dd89f5a8af9a622dc72e
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-05 13:33:59 +0300

    footer-update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={footer_logo }alt="" />
+                <p>I am a tech enthusiast with a strong belief in the transformative power of technology to shape the future. </p>
+            </div>
+        </div>
+    </div>
   )
 }
 

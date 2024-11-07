0fd17e66f240075ee0529e4825a3aab5c1e6382d
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-07 17:18:41 +0300

    resume update

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,8 +1,8 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
+import project1_img from '../assets/work-1.png'
+import project2_img from '../assets/work-2.png'
+import project3_img from '../assets/work-3.png'
+import project4_img from '../assets/work-4.png'
+import project5_img from '../assets/work-5.png'
 import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
